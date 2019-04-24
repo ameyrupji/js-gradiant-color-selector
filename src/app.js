9088fb581417751ce548ba236099fbd1c28e5a78
Amey Rupji <ameyrupji@gmail.com>
2019-04-24 14:20:19 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,67 @@
+# credits: https://stackoverflow.com/questions/30143082/how-to-get-color-value-from-gradient-by-percentage-with-javascript
+
+var gradient = [
+    [
+        0,
+        [53, 92, 125]
+    ],
+    [
+        25,
+        [247, 219, 79]
+    ],
+    [
+        50,
+        [53, 92, 125]
+    ],
+    [
+        75,
+        [0,0,0]
+    ],
+    [
+        100,
+        [53, 92, 125]
+    ]
+];
+
+var sliderWidth = 500;
+
+$( "#slider" ).slider({
+    min: 1,
+    slide: function( event, ui ) {
+        
+        var colorRange = []
+        $.each(gradient, function( index, value ) {
+            if(ui.value<=value[0]) {
+                colorRange = [index-1,index]
+                return false;
+            }
+        });
+        
+        //Get the two closest colors
+        var firstcolor = gradient[colorRange[0]][1];
+        var secondcolor = gradient[colorRange[1]][1];
+        
+        //Calculate ratio between the two closest colors
+        var firstcolor_x = sliderWidth*(gradient[colorRange[0]][0]/100);
+        var secondcolor_x = sliderWidth*(gradient[colorRange[1]][0]/100)-firstcolor_x;
+        var slider_x = sliderWidth*(ui.value/100)-firstcolor_x;
+        var ratio = slider_x/secondcolor_x
+        
+        //Get the color with pickHex(thx, less.js's mix function!)
+        var result = pickHex( secondcolor,firstcolor, ratio );
+                       
+        $('#result').css("background-color", 'rgb('+result.join()+')');
+        
+    }
+});
+
+function pickHex(color1, color2, weight) {
+    var p = weight;
+    var w = p * 2 - 1;
+    var w1 = (w/1+1) / 2;
+    var w2 = 1 - w1;
+    var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
+        Math.round(color1[1] * w1 + color2[1] * w2),
+        Math.round(color1[2] * w1 + color2[2] * w2)];
+    return rgb;
+}
